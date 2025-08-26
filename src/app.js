e5a0653cac3566d785dbd766dc128e3dd0ed6d14
Eylinsmc <133468726+Eylinsmc@users.noreply.github.com>
2025-08-26 00:46:08 +0000

    Domain Generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+  let domainName = document.getElementById("domainName")
+
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let ext = ['.com', '.net', '.es']
+  
+  let pronounRandom = Math.floor(Math.random() * pronoun.length)
+
+  let adjRandom = Math.floor(Math.random() * adj.length)
+  
+  let nounRandom = Math.floor(Math.random() * noun.length)
+
+  let extRandom = Math.floor(Math.random() * ext.length)
+
+  domainName.innerHTML = `${pronoun[pronounRandom]}${adj[adjRandom]}${noun[nounRandom]}${ext[extRandom]}`
+  
+}
+
+
